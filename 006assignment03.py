8eaba02ef13f2b8a3a4db04f6be57204b00c58e8
JAYASHKUMAR <68191917+NAMANORJAYASH@users.noreply.github.com>
2021-12-12 15:05:30 -0500

    Update 006assignment03.py

--- 006assignment03.py
+++ 006assignment03.py
@@ -1,6 +1,6 @@
 h1=int(input("Give me the start time hour:"))
 m1=int(input("Give me the start time minute:"))
-d=int(input("Give me the duration in minutes"))
+d=int(input("Give me the duration in minutes:"))
 f=h1*60+m1+d
 h2=f//60
 m2=f-h2*60
